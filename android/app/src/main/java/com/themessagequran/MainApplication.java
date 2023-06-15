8dcc35cf1dd426b25dc504f1bed93b488eaa800d
thesammet <50338990+thesammet@users.noreply.github.com>
2023-06-16 00:59:47 +0300

    fix: android scrollable-tab bug

--- android/app/src/main/java/com/themessagequran/MainApplication.java
+++ android/app/src/main/java/com/themessagequran/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -21,13 +22,11 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          packages.add(new RNCViewPagerPackage());
           return packages;
         }
-
+        
         @Override
         protected String getJSMainModuleName() {
           return "index";
